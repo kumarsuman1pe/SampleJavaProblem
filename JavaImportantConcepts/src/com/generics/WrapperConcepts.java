b99b4dfd7ef92e6d3eb06bfafb1f468572121a79
Suman kumar <sumankumar@Sumans-MacBook-Pro.local>
2022-06-14 14:38:38 +0530

    Adding Wrapper class

--- JavaImportantConcepts/src/com/generics/WrapperConcepts.java
+++ JavaImportantConcepts/src/com/generics/WrapperConcepts.java
@@ -1,5 +1,12 @@
 package com.generics;
-
+/***
+ * 
+ * @author sumankumar
+ *
+ */
 public class WrapperConcepts {
-
+	/***
+	 * The wrapper class in Java provides the mechanism to convert primitive into
+	 * object and object into primitive.
+	 */
 }
