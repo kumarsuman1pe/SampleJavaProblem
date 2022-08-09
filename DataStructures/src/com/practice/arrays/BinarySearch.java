952ee7fd7700465f7de483f184d4f430e48a1962
Suman kumar <sumankumar@Sumans-MacBook-Pro.local>
2022-08-09 18:58:55 +0530

    Added Binary and Linear Search code

--- DataStructures/src/com/practice/arrays/BinarySearch.java
+++ DataStructures/src/com/practice/arrays/BinarySearch.java
@@ -6,8 +6,58 @@ package com.practice.arrays;
 public class BinarySearch {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		int array[] = { 10, 11, 23, 44, 56, 66, 77, 99,100 ,105};
+		int key = 99;
+		int elementFoundIndex = IbinarySearch(array, key);
+		if (elementFoundIndex != -1) {
+			System.out.println("Element found at index: " + elementFoundIndex);
+		} else {
+			System.out.println("Element not found! ");
+
+		}
+		
+		
+		
+		int key1 = 66;
+		int elementFoundIndexByRec = RbinarySearch(array,0,array.length-1, key1);
+		if (elementFoundIndexByRec != -1) {
+			System.out.println("Element found at index: " + elementFoundIndexByRec);
+		} else {
+			System.out.println("Element not found! ");
+
+		}
+	}
+
+	private static int RbinarySearch(int[] array, int low, int high, int key) {
+		
+		if (low <= high) {
+			int mid = (low + high) / 2;
+			if (key == array[mid]) {
+				return mid;
+			} else if (key < array[mid]) {
+				return RbinarySearch( array,low,mid -1,key);
+			} else {
+				return RbinarySearch( array,mid+1,high,key);
+			}
+		}
+		return -1;
+	}
+
+	private static int IbinarySearch(int[] array, int key) {
+		int high = array.length-1;
+		int low = 0;
+		while (low <= high) {
+			int mid = (low + high) / 2;
+			if (key == array[mid]) {
+				return mid;
+			} else if (key < array[mid]) {
+				high = mid -1;
+			} else {
+				low = mid + 1;
+			}
+		}
+		return -1;
 	}
 
 }
--- DataStructures/src/com/practice/arrays/LinearSearch.java
+++ DataStructures/src/com/practice/arrays/LinearSearch.java
@@ -1,15 +1,92 @@
 package com.practice.arrays;
 
+import java.util.List;
+
 /*
  * write  all three version of Linear Search
  * Normal
  * Enhanced Version: Transposition And Move to front
  */
 public class LinearSearch {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int array[] = { 1, 11, 23, 44, 55, 66 };
 
+		int index = linearSearch(array, 55);
+
+		if (index != -1) {
+			System.out.println("Element found at index: " + index);
+		} else {
+			System.out.println("Element not found in given array: ");
+
+		}
+
+		int transposeIndex = transposelLnearSearch(array, 66);
+
+		if (transposeIndex != -1) {
+			System.out.println("Element found at index: " + transposeIndex);
+		} else {
+			System.out.println("Element not found in given array: ");
+
+		}
+		// move to Head
+
+		int moveToZeroIndex = moveToZeroLnearSearch(array, 11);
+
+		if (transposeIndex != -1) {
+			System.out.println("Element found at index: " + moveToZeroIndex);
+		} else {
+			System.out.println("Element not found in given array: ");
+
+		}
+
+	}
+
+	private static int moveToZeroLnearSearch(int[] array, int key) {
+
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] == key) {
+				swapWithIndexZero(array, i);
+				return i;
+			}
+		}
+
+		return -1;
+	}
+
+	private static void swapWithIndexZero(int[] array, int i) {
+		int temp = array[i];
+		array[i] = array[0];
+		array[0] = temp;
+	}
+
+	private static int transposelLnearSearch(int[] array, int key) {
+		int foundAtIndex = -1;
+
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] == key) {
+				swap(array, i);
+				return i;
+			}
+		}
+
+		return foundAtIndex;
+	}
+
+	private static void swap(int array[], int i) {
+		int temp = array[i];
+		array[i] = array[i - 1];
+		array[i - 1] = temp;
+	}
+
+	private static int linearSearch(int[] array, int key) {
+		int foundAtIndex = -1;
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] == key) {
+				foundAtIndex = i;
+			}
+
+		}
+		return foundAtIndex;
 	}
 
 }
